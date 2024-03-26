0532e9286a785a224630323643357263887f0d55
Alberto Cangialosi <billyballo.res@gmail.com>
2024-03-26 15:54:53 +0100

    feat(api): prelimiary GET /film/id

--- server.js
+++ server.js
@@ -8,12 +8,48 @@ app.use(express.json());
 
 const connection = mysql.createConnection({
   host: 'localhost',
-  user: 'pma',
+  user: 'root',
   database: 'movie-blog-app'
 });
 
 app.get('/', async (req, res) => {
   res.json({ message: 'Welcome to the Movie Blog API' });
+});
+
+// DEBUG :
+console.log('connection', connection);
+
+app.get('/films/:id', async (req, res) => {
+
+  const filmId = req.params.id;
+  const sql = `SELECT * FROM film WHERE id = ?`;
+
+  try {
+    const [results, fields] = await connection.execute(sql, [filmId]);
+
+    if (results.length === 0) {
+      return res.status(404).json({ error: 'Film not found' });
+    }
+
+    const film = results[0];
+
+    const cast = JSON.parse(film.cast || '[]');
+
+    const parsedFilm = {
+      id: filmId,
+      title: film.title,
+      year: film.year,
+      image: film.image,
+      director: film.director,
+      cast: cast,
+      genre: film.genre
+    };
+
+    res.json(parsedFilm);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
 });
 
 app.listen(PORT, () => {

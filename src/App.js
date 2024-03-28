40a87cce1d2af48440fbf75cb82d43c25f6f4473
alexrusso97 <alexrusso971@outlook.it>
2024-03-28 10:39:41 +0100

    feat(ui): create footer

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import Navbar from './components/Navbar';
-import './App.css';
+import Footer from './components/Footer';
 
 function App() {
   return (
+    <>
     <Navbar />
+    <Footer />
+    </>
   );
 }
 

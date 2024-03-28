74e9d6b1cdc39f2adf7e19aaa6eed8bfca6f82ba
Alberto Cangialosi <billyballo.res@gmail.com>
2024-03-28 23:52:31 +0100

    feat(js): trying to fetchMovies and setItem in Storage

--- src/Context.jsx
+++ src/Context.jsx
@@ -1,18 +1,50 @@
-import { createContext, useContext, useState } from "react";
+import { createContext, useContext, useEffect, useState } from "react";
 
 const AppContext = createContext({
 	loggedUser: null,
-	setLoggedUser: () => { },
+	setLoggedUser: () => {},
 	movies: [],
-	setMovies: () => { },
+	setMovies: () => {},
 	reviews: [],
-	setReviews: () => { },
+	setReviews: () => {},
 });
 
 const AppProvider = ({ children }) => {
 	const [loggedUser, setLoggedUser] = useState(null);
-	const [movies, setMovies] = useState([]);
+	const [movies, setMovies] = useState(
+		!!localStorage.getItem("movies")
+			? JSON.parse(localStorage.getItem("movies"))
+			: []
+	);
 	const [reviews, setReviews] = useState([]);
+
+	async function utilityFetchMovies() {
+		try {
+			const response = await fetch("http://localhost:8080/films");
+
+			if (!response.ok) {
+				// DEBUG :
+				const errorMessage = await response.text();
+				console.error("Server responded with error:", errorMessage);
+
+				throw new Error("Network response was not ok");
+			}
+
+			const data = await response.json();
+			localStorage.setItem("movies", JSON.stringify(data.films));
+			return data.films;
+		} catch (error) {
+			console.error("Error fetching movies:", error);
+			throw error;
+		}
+	}
+
+	useEffect(() => {
+		if (!movies.length) {
+			const fetchedMovies = utilityFetchMovies();
+			setMovies(fetchedMovies);
+		}
+	}, []);
 
 	return (
 		<AppContext.Provider

3d721b2c5e0b0db30405e607ed8283e940279ab9
alexrusso97 <alexrusso971@outlook.it>
2024-03-29 22:17:32 +0100

    feat(js): added localStorage removal of reviews when moving the page.

--- src/routes/About.jsx
+++ src/routes/About.jsx
@@ -1,4 +1,11 @@
+import { useEffect } from "react";
+
 export function About() {
+
+	useEffect(() => {
+        localStorage.removeItem("reviews");
+    }, []);
+	
 	return (
 		<div>
 			<h1>About</h1>

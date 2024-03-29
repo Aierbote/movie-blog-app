5ec6d5000f2f9e3f9f139ba94d782b4db59ca7b7
Alberto Cangialosi <billyballo.res@gmail.com>
2024-03-29 14:31:04 +0100

    feat(js): a more dynamic router
    - add an array `routes`
    - implement `<Routes>` with `routes.map()`
    - get parameter `:id` from `/movie/:id` with hook `useParams()`

--- src/App.js
+++ src/App.js
@@ -1,10 +1,34 @@
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Navigate, useParams } from 'react-router-dom';
+import { useEffect, useState } from 'react';
 
+
+const routes = [
+  {
+    path: '/',
+    component: Home,
+    exact: true,
+  },
+  {
+    path: '/about',
+    component: About,
+    exact: false,
+  },
+  {
+    path: '/movie/:id',
+    component: MoviePage,
+    exact: false,
+  },
+  {
+    path: '*',
+    component: NotFound,
+    exact: false,
+  },
+]
+
+// TODO : preliminary implementation of page/route components
 function Home() {
-  console.log('Home component rendered');
-
   return (
     <div>
       <h1>Home</h1>
@@ -13,8 +37,6 @@ function Home() {
 }
 
 function About() {
-  console.log('About component rendered');
-
   return (
     <div>
       <h1>About</h1>
@@ -23,11 +45,11 @@ function About() {
 }
 
 function MoviePage() {
-  console.log('Movie component rendered');
+  const { id: idFilm } = useParams();
 
   return (
     <div>
-      <h1>Movie Page</h1>
+      <h1>Movie Page {idFilm}</h1>
     </div>
   );
 }
@@ -36,8 +58,7 @@ function NotFound() {
   return (
     <div>
       <h1>404 Not Found</h1>
-      {/* NOTE : Redirect after 3 seconds */}
-      {setTimeout(() => <Navigate to="/" />, 3_000)}
+      <p>Redirecting in 5 seconds...</p>
     </div>
   );
 }
@@ -49,11 +70,14 @@ function App() {
         <Navbar />
 
         <Routes>
-          <Route path="/" exact element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/movie/:id" element={<MoviePage />} />
-          {/* NOTE : this must be ALWAYS the last one */}
-          <Route path="*" element={<NotFound />} />
+          {routes.map((route, index) => (
+            <Route
+              key={index}
+              path={route.path}
+              element={<route.component />}
+              exact={route.exact}
+            />
+          ))}
         </Routes>
 
         <Footer />

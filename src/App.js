0ed9c6c8861431f05a3da0c9925aaff995af407e
Alberto Cangialosi <billyballo.res@gmail.com>
2024-03-29 12:41:32 +0100

    feat(js): add static router

--- src/App.js
+++ src/App.js
@@ -1,11 +1,63 @@
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+
+function Home() {
+  console.log('Home component rendered');
+
+  return (
+    <div>
+      <h1>Home</h1>
+    </div>
+  );
+}
+
+function About() {
+  console.log('About component rendered');
+
+  return (
+    <div>
+      <h1>About</h1>
+    </div>
+  );
+}
+
+function MoviePage() {
+  console.log('Movie component rendered');
+
+  return (
+    <div>
+      <h1>Movie Page</h1>
+    </div>
+  );
+}
+
+function NotFound() {
+  return (
+    <div>
+      <h1>404 Not Found</h1>
+      {/* NOTE : Redirect after 3 seconds */}
+      {setTimeout(() => <Navigate to="/" />, 3_000)}
+    </div>
+  );
+}
 
 function App() {
   return (
     <>
-    <Navbar />
-    <Footer />
+      <BrowserRouter>
+        <Navbar />
+
+        <Routes>
+          <Route path="/" exact element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/movie/:id" element={<MoviePage />} />
+          {/* NOTE : this must be ALWAYS the last one */}
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+
+        <Footer />
+      </BrowserRouter>
     </>
   );
 }

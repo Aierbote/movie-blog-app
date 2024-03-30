9b1f7c5ba92aa48da0a8d04aa55647a12a9eae6f
Alberto Cangialosi <billyballo.res@gmail.com>
2024-03-30 09:58:07 +0100

    refactor(js): cleanup for reviews in state e in storage

--- src/routes/About.jsx
+++ src/routes/About.jsx
@@ -1,11 +1,4 @@
-import { useEffect } from "react";
-
 export function About() {
-
-	useEffect(() => {
-        localStorage.removeItem("reviews");
-    }, []);
-	
 	return (
 		<div>
 			<h1>About</h1>

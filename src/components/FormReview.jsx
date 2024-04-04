e81d1e6c77aa4061d2ca452edc215a05c27fb618
Alberto Cangialosi <billyballo.res@gmail.com>
2024-04-04 20:49:50 +0200

    fix(js): handleDelete truly works with db and ui

--- src/components/FormReview.jsx
+++ src/components/FormReview.jsx
@@ -1,14 +1,111 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { TextField, Button, Rating, Typography, Box } from "@mui/material";
 import { useAppContext } from "../Context";
 
-const FormReview = ({ idFilm }) => {
+const FormReview = ({ idFilm, editing = false, setEditing = () => {} }) => {
 	const [comment, setComment] = useState("");
 	const [rating, setRating] = useState(0);
-	const { setReviews, loggedUser } = useAppContext();
+	// id for the review to edit
+	const [editingId, setEditingId] = useState(-1);
+	// author for the review to edit
+	const [editingAuthor, setEditingAuthor] = useState(null);
+	const { reviews, setReviews, loggedUser } = useAppContext();
+
+	function handleCancel() {
+		setEditing(false);
+	}
+
+	const handleUpdate = async (e) => {
+		e.preventDefault();
+
+		try {
+			const response = await fetch(
+				`http://localhost:8080/films/${idFilm}/reviews/${editingId}`,
+				{
+					method: "PUT",
+					headers: {
+						"Content-Type": "application/json",
+					},
+					body: JSON.stringify({
+						comment,
+						rating,
+						user: editingAuthor,
+					}),
+				}
+			);
+
+			if (response.ok) {
+				console.log(response);
+
+				// To update the reviews by updating the review with the same id
+				setReviews((prevReviews) =>
+					prevReviews.map((review) =>
+						review.idReview === editingId
+							? { ...review, comment, rating }
+							: review
+					)
+				);
+			}
+		} catch (error) {
+			console.error(`Error updating review: ${error}`);
+		}
+	};
+
+	const handleDelete = async (e) => {
+		e.preventDefault();
+
+		try {
+			const response = await fetch(
+				`http://localhost:8080/films/${idFilm}/reviews/${editingId}`,
+				{
+					method: "DELETE",
+					headers: {
+						"Content-Type": "application/json",
+					},
+				}
+			);
+
+			if (response.ok) {
+				console.log(response);
+
+				// To delete the review with the same id
+				setReviews((prevReviews) =>
+					prevReviews.filter((review_) => review_.idReview !== editingId)
+				);
+			}
+		} catch (error) {
+			console.error(`Error deleting review: ${error}}`);
+		}
+
+		const oldReviews = JSON.parse(localStorage.getItem("reviews")) || [];
+		const updatedReviews = oldReviews.filter(
+			(review_) => review_.idReview !== editingId
+		);
+		localStorage.setItem("reviews", JSON.stringify(updatedReviews));
+	};
+
+	useEffect(() => {
+		if (editing) {
+			const editingReviews = reviews.find(
+				(review) => review.idFilm === idFilm && review.user === loggedUser
+			);
+
+			console.log("editingReviews", editingReviews);
+
+			setComment(editingReviews.comment);
+			setRating(editingReviews.rating);
+			setEditingId(editingReviews.idReview);
+		}
+	}, [editing]);
 
 	const handleSubmit = async (e) => {
 		e.preventDefault();
+
+		if (editing) {
+			handleUpdate(e);
+			setEditing(false);
+			return;
+		}
 
 		try {
 			const response = await sendReview();
@@ -68,6 +165,9 @@ const FormReview = ({ idFilm }) => {
 			);
 			const updatedReviews = await updatedResponse.json();
 			setReviews(updatedReviews);
+
+			// usual response of POST
+			return response.json();
 		} catch (error) {
 			console.error("Error sending review:", error);
 			throw error;
@@ -86,6 +186,21 @@ const FormReview = ({ idFilm }) => {
 			borderColor={"primary.main"}
 		>
 			<form onSubmit={handleSubmit}>
+				{editing && (
+					<Box display={"flex"} justifyContent={"flex-end"}>
+						<Button
+							type="submit"
+							variant="contained"
+							color="success"
+							onClick={() => {
+								setEditing(false);
+							}}
+							disabled={!isRatingValid || !isCommentValid}
+						>
+							SAVE
+						</Button>
+					</Box>
+				)}
 				<Box
 					mb={2}
 					border={2}
@@ -113,14 +228,32 @@ const FormReview = ({ idFilm }) => {
 					margin="normal"
 				/>
 				<br />
-				<Button
-					type="submit"
-					variant="contained"
-					color="primary"
-					disabled={!isRatingValid || !isCommentValid}
-				>
-					Invia recensione
-				</Button>
+				{!editing && (
+					<Button
+						type="submit"
+						variant="contained"
+						color="primary"
+						disabled={!isRatingValid || !isCommentValid}
+					>
+						Invia recensione
+					</Button>
+				)}
+				{editing && (
+					<Box
+						display="flex"
+						flexDirection="row"
+						justifyContent="flex-end"
+						gap="1rem"
+						margin={1}
+					>
+						<Button variant="outlined" onClick={handleCancel}>
+							Cancel
+						</Button>
+						<Button variant="contained" color="error" onClick={handleDelete}>
+							DELETE
+						</Button>
+					</Box>
+				)}
 			</form>
 		</Box>
 	);

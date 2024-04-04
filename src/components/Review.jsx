562385e70d04bf44f62bf8dd31ad6a42780f047d
Alberto Cangialosi <billyballo.res@gmail.com>
2024-04-04 16:10:00 +0200

    feat(js): add handleDetele() and handleUpdate()

--- src/components/Review.jsx
+++ src/components/Review.jsx
@@ -7,9 +7,90 @@ import Typography from "@mui/material/Typography";
 import Rating from "@mui/material/Rating";
 import Button from "@mui/material/Button";
 import Box from "@mui/material/Box";
+import { useState, useEffect } from "react";
+import { useAppContext } from "../Context";
 
 const Review = ({ review, reviewLeft, loggedUser }) => {
 	const { idReview, idFilm, rating, comment, user: author } = review;
+	const { reviews, setReviews } = useAppContext();
+	const [editing, setEditing] = useState(false);
+
+	function handleEdit() {
+		setEditing(true);
+	}
+
+	function handleCancel() {
+		setEditing(false);
+	}
+
+	const handleUpdate = async (e) => {
+		e.preventDefault();
+
+		try {
+			const response = await fetch(
+				`http://localhost:8080/films/${idFilm}/reviews/${idReview}`,
+				{
+					method: "PUT",
+					headers: {
+						"Content-Type": "application/json",
+					},
+					body: JSON.stringify({
+						comment,
+						rating,
+						user: author,
+					}),
+				}
+			);
+
+			if (response.ok) {
+				console.log(response);
+
+				// To update the reviews by updating the review with the same id
+				setReviews((prevReviews) =>
+					prevReviews.map((review) =>
+						review.idReview === idReview
+							? { ...review, comment, rating }
+							: review
+					)
+				);
+			}
+		} catch (error) {
+			console.error(`Error updating review: ${error}`);
+		}
+	};
+
+	const handleDelete = async (e) => {
+		e.preventDefault();
+
+		try {
+			const response = await fetch(
+				`http://localhost:8080/films/${idFilm}/reviews/${idReview}`,
+				{
+					method: "DELETE",
+					headers: {
+						"Content-Type": "application/json",
+					},
+				}
+			);
+
+			if (response.ok) {
+				console.log(response);
+
+				// To delete the review with the same id
+				setReviews((prevReviews) =>
+					prevReviews.filter((review_) => review_.idReview !== idReview)
+				);
+			}
+		} catch (error) {
+			console.error(`Error deleting review: ${error}}`);
+		}
+
+		const oldReviews = JSON.parse(localStorage.getItem("reviews")) || [];
+		const updatedReviews = oldReviews.filter(
+			(review_) => review_.idReview !== idReview
+		);
+		localStorage.setItem("reviews", JSON.stringify(updatedReviews));
+	};
 
 	return (
 		<>

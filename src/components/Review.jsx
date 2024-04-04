93603fe5443b1e63a8a77ece3ebe110099f8c33c
Alberto Cangialosi <billyballo.res@gmail.com>
2024-04-04 20:49:50 +0200

    fix(js): handleDelete truly works with db and ui

--- src/components/Review.jsx
+++ src/components/Review.jsx
@@ -16,85 +16,10 @@ const Review = ({ review, reviewLeft, loggedUser }) => {
 	const { reviews, setReviews } = useAppContext();
 	const [editing, setEditing] = useState(false);
 
-	function toggleEdit() {
-		setEditing((prevEditing) => !prevEditing);
-	}
-
-	function handleCancel() {
-		setEditing(false);
-	}
-
-	const handleUpdate = async (e) => {
-		e.preventDefault();
-
-		try {
-			const response = await fetch(
-				`http://localhost:8080/films/${idFilm}/reviews/${idReview}`,
-				{
-					method: "PUT",
-					headers: {
-						"Content-Type": "application/json",
-					},
-					body: JSON.stringify({
-						comment,
-						rating,
-						user: author,
-					}),
-				}
-			);
-
-			if (response.ok) {
-				console.log(response);
-
-				// To update the reviews by updating the review with the same id
-				setReviews((prevReviews) =>
-					prevReviews.map((review) =>
-						review.idReview === idReview
-							? { ...review, comment, rating }
-							: review
-					)
-				);
-			}
-		} catch (error) {
-			console.error(`Error updating review: ${error}`);
-		}
-	};
-
-	const handleDelete = async (e) => {
-		e.preventDefault();
-
-		try {
-			const response = await fetch(
-				`http://localhost:8080/films/${idFilm}/reviews/${idReview}`,
-				{
-					method: "DELETE",
-					headers: {
-						"Content-Type": "application/json",
-					},
-				}
-			);
-
-			if (response.ok) {
-				console.log(response);
-
-				// To delete the review with the same id
-				setReviews((prevReviews) =>
-					prevReviews.filter((review_) => review_.idReview !== idReview)
-				);
-			}
-		} catch (error) {
-			console.error(`Error deleting review: ${error}}`);
-		}
-
-		const oldReviews = JSON.parse(localStorage.getItem("reviews")) || [];
-		const updatedReviews = oldReviews.filter(
-			(review_) => review_.idReview !== idReview
-		);
-		localStorage.setItem("reviews", JSON.stringify(updatedReviews));
-	};
-
 	if (editing) {
-		return <FormReview />;
+		return (
+			<FormReview idFilm={idFilm} editing={editing} setEditing={setEditing} />
+		);
 	}
 
 	return (
@@ -121,10 +46,9 @@ const Review = ({ review, reviewLeft, loggedUser }) => {
 							{loggedUser === author && reviewLeft && (
 								<Button
 									variant="contained"
-									// color="primary"
-									color={!editing ? "primary" : "success"}
+									color="primary"
 									onClick={() => {
-										toggleEdit();
+										setEditing(true);
 									}}
 								>
 									{!editing && "Edit..."}
@@ -141,23 +65,6 @@ const Review = ({ review, reviewLeft, loggedUser }) => {
 					}
 				/>
 			</ListItem>
-
-			{editing && (
-				<Box
-					display="flex"
-					flexDirection="row"
-					justifyContent="flex-end"
-					gap="1rem"
-					margin={1}
-				>
-					<Button variant="outlined" onClick={handleCancel}>
-						Cancel
-					</Button>
-					<Button variant="contained" color="error" onClick={handleDelete}>
-						DELETE
-					</Button>
-				</Box>
-			)}
 
 			<Divider variant="inset" component="li" />
 		</>

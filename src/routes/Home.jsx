be6e9b9654d27252ddd97fd7eebcdbcc09dd9e29
Alberto Cangialosi <billyballo.res@gmail.com>
2024-04-04 23:28:58 +0200

    docs: update functionalies description

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -5,25 +5,36 @@ export function Home() {
 	return (
 		<div>
 			<Box
-				margin={'auto'}
-				height={'auto'}
-				width={'90%'}
+				margin={"auto"}
+				height={"auto"}
+				width={"90%"}
 				my={4}
 				display="flex"
-				flexDirection={'column'}
+				flexDirection={"column"}
 				alignItems="flex-start"
 				justifyContent="center"
 				gap={4}
 				p={4}
-				sx={{ border: '2px solid grey', borderRadius: '10px'}}
+				sx={{ border: "2px solid grey", borderRadius: "10px" }}
 			>
-					<Typography fontSize={25} fontFamily={'fantasy'}> Benvenuto cinefilo! </Typography>
-					<Typography variant="h6">Questo è il blog per eccellenza per te che sei patito di cinema e ami condividere con gli altri le tue opinioni.</Typography>
-					<Typography variant="h6">Qui potrai sbizzarrirti tra i titoli del momento e i grandi classici!
-					Seleziona il film per vedere le informazioni e registrati per recensire ed unirti alla nostra community.</Typography>
-					<Typography variant="h6" fontFamily={'inherit'}>Prima regola del MovieBlog: mai parlare del MovieBlog!</Typography>
-
-				</Box>
+				<Typography fontSize={25} fontFamily={"fantasy"}>
+					{" "}
+					Benvenuto cinefilo!{" "}
+				</Typography>
+				<Typography variant="h6">
+					Questo è il blog per eccellenza per te che sei patito di cinema e ami
+					condividere con gli altri le tue opinioni.
+				</Typography>
+				<Typography variant="h6">
+					Qui potrai sbizzarrirti tra i titoli del momento e i grandi classici!
+					Seleziona il film per vedere le informazioni e registrati per
+					recensire ed unirti alla nostra community. Sei libero ti modificare o
+					cancella la tua recensione in un secondo momento.
+				</Typography>
+				<Typography variant="h6" fontFamily={"inherit"}>
+					Prima regola del MovieBlog: mai parlare del MovieBlog!
+				</Typography>
+			</Box>
 
 			<TitlebarImageList />
 		</div>
